7bab2f3a5e86c8f2c95bfae2ac1cbb14b2feb906
SandersWangBin <sanderswangbin@gmail.com>
2017-06-12 14:06:25 +0200

    The 1st example to check JSON. - The function checkUseRegex: check the JSON using regular expression. - Plan to create the another function: checkUsePull to simplify the JSON checks logics.

--- test/com/json/check/JsonCheck.java
+++ test/com/json/check/JsonCheck.java
@@ -4,17 +4,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class JsonCheck {
-	final static String JSON_EXAMPLE_01 = "{\"message\": \"Created example object \'ExampleObj01\'\"}";
-	final static String PULL_EXAMPLE_01 = "p\'\\{\"message\": \"Created.*\'([a-zA-Z0-9_-]+)\'\"\\}\'.PULL()";
-	final static String REG_EXAMPLE_01 = "\\{\"message\": \"Created.*\'([a-zA-Z0-9_-]+)\'\"\\}";
+	final static String JSON_EXAMPLE_01_TRUE = "{\"message\": \"Created example object \'ExampleObj01\'\"}";
+	final static String JSON_EXAMPLE_01_FALSE = "{\"message\": \"Failed to created \'ExampleObj01\'\"}";
+	final static String PULL_EXAMPLE_01 = "p\'\\{\"message\": \"([a-zA-Z]+)\\s.*\'([a-zA-Z0-9_-]+)\'\"\\}\'.PULL({0}==\"Created\";{1}==\"ExampleObj01\")";
+	final static String REG_EXAMPLE_01 = "\\{\"message\": \"([a-zA-Z]+)\\s.*\'([a-zA-Z0-9_-]+)\'\"\\}";
+	final static String[] REG_EXAMPLE_01_CHECKS = {"Created", "ExampleObj01"};
 
-	public static void main(String... argv) {
-		Pattern p = Pattern.compile(REG_EXAMPLE_01);
-		Matcher m = p.matcher(JSON_EXAMPLE_01);
+	public boolean checkUseRegex(String regex, String text, String[] checks) {
+		boolean result = true;
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(text);
 		if (m.find()) {
 			for (int i = 1; i <=m.groupCount(); i++) {
-				System.out.println(m.group(i));
+				result = result && (checks[i-1].equals(m.group(i)));
 			}
+		} else {
+			result = false;
 		}
+		return result;
+	}
+
+	public static void main(String... argv) {
+		JsonCheck jck = new JsonCheck();
+		System.out.println(jck.checkUseRegex(REG_EXAMPLE_01, JSON_EXAMPLE_01_TRUE, REG_EXAMPLE_01_CHECKS));
+		System.out.println(jck.checkUseRegex(REG_EXAMPLE_01, JSON_EXAMPLE_01_FALSE, REG_EXAMPLE_01_CHECKS));
 	}
 }
