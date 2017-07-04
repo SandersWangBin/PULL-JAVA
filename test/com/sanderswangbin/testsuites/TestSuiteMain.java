b2d4134b243f42cc370ca96db3099f25677138e1
SandersWangBin <sanderswangbin@gmail.com>
2017-07-04 12:39:03 +0200

    PULL: move out JSONCK part to JSONCK-JAVA project
    Signed-off-by: SandersWangBin <sanderswangbin@gmail.com>

--- test/com/sanderswangbin/testsuites/TestSuiteMain.java
+++ test/com/sanderswangbin/testsuites/TestSuiteMain.java
@@ -1,6 +1,5 @@
 package com.sanderswangbin.testsuites;
 
-import com.sanderswangbin.testcases.jsonck.TestCaseJsonCK;
 import com.sanderswangbin.testcases.pull.TestCasePullChain;
 import com.sanderswangbin.testcases.pull.TestCasePullRegexp;
 
@@ -9,7 +8,6 @@ public class TestSuiteMain {
 	public static void main(String... argv) {
 		System.out.println(new TestCasePullRegexp().test());
 		System.out.println(new TestCasePullChain().test());
-		System.out.println(new TestCaseJsonCK().test());
 	}
 
 }
